7bcbe157efb56fb2d402ca292f80b79c1698f9c4
Gan <kygan438@gmail.com>
2026-04-14 01:03:00 +0800

    Remove merge conflict artifacts

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 from flask import Flask, render_template
-<<<<<<< HEAD
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Database/project.db"
-=======
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -19,7 +14,6 @@ db.init_app(app)
 import database
 with app.app_context():
     db.create_all()
->>>>>>> b05141f (Completed initial database configuration)
 
 @app.route("/")
 def index():
--- database.py
+++ database.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from app import app
-from flask_sqlalchemy import flasksql
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    pass
-
-Database = flasksql(app, model_class = Base)
-=======
 from app import db
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
@@ -16,5 +6,3 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key = True)
     username: Mapped[str] = mapped_column(unique = True)
     email: Mapped[str]
-
->>>>>>> b05141f (Completed initial database configuration)
