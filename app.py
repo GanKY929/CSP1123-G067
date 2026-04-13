ad376fe2cd444030f1b706655f4b3e25ce14cddb
Gan <kygan438@gmail.com>
2026-04-14 00:46:27 +0800

    Squashed commit of the following:
    commit ae1b32588dd0946555a814b1e01507a2d480b9cc
    Author: Gan <kygan438@gmail.com>
    Date:   Mon Apr 13 18:45:48 2026 +0800
    
        clear cache, gitignore, delete unusable html
    
    commit cead3e41f4f9b310f463cdbd5ab8387940a45ac0
    Author: Gan <kygan438@gmail.com>
    Date:   Mon Apr 13 16:39:54 2026 +0800
    
        clear cache, gitignore
    
    commit 05a08c43c030734c39ba6206cde47f27aa61eba3
    Author: Mithirilz <haqimiamirul829@gmail.com>
    Date:   Mon Apr 13 14:39:17 2026 +0800
    
        Updated requirements.txt
    
    commit c48234d360acdf1a6a4b8df0094048b495cec292
    Author: Mithirilz <haqimiamirul829@gmail.com>
    Date:   Mon Apr 13 14:23:27 2026 +0800
    
        Minor code error fixes
    
    commit 08b2148c40f923074618f573907c7204f43ad3b0
    Author: Mithirilz <haqimiamirul829@gmail.com>
    Date:   Mon Apr 13 14:20:51 2026 +0800
    
        database setup
    
    commit a88b719999893f0421d8c0a1c6622513018d9653
    Author: WTH <wahth23@gmail.com>
    Date:   Sun Apr 12 11:49:53 2026 +0800
    
        Update index.html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Database/project.db"
 
 @app.route("/")
 def index():
---
+++ database.py
@@ -0,0 +1,8 @@
+from app import app
+from flask_sqlalchemy import flasksql
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+Database = flasksql(app, model_class = Base)
