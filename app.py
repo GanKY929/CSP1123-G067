9aeb4cb0c29b276c73c0932f154f339edc8c836d
Gan <kygan438@gmail.com>
2026-04-15 19:50:43 +0800

    login and signup functionality; add validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -15,17 +15,63 @@ import database
 with app.app_context():
     db.create_all()
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/login")
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        return doLogin(username, password)
     return render_template("login.html")
 
-@app.route("/signup")
+
+@app.route("/doLogin" , methods=["POST"])
+def doLogin(username, password):
+        #database query to check if username and password are correct
+
+        return render_template("login.html", username=username)
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirmPassword")
+        return doSignup(username, email, password, confirmPassword)
     return render_template("signup.html")
+
+
+def doSignup(username, email, password, confirmPassword):
+    try:
+        if password.lower() == password:
+            raise Exception("password cannot be all lowercase.")
+        if password.upper() == password:
+            raise Exception("password cannot be all uppercase.")
+        for char in password:
+            if char.isspace():
+                raise Exception("password cannot contain spaces.")
+        if password != confirmPassword:
+            raise Exception("Passwords do not match.")
+        if len(password) < 8:
+            raise Exception("password must be at least 8 characters long.")
+        if db.session.query(                ).filter_by(username=username).first():
+            raise Exception("username already exists.")
+        if db.session.query(                ).filter_by(email=email).first():
+            raise Exception("email already exists.")
+        #new_user = (username=username, email=email, password=password)
+        #db.session.add(new_user)
+        db.session.commit()
+
+    except Exception as e:
+        return render_template("signup.html", username=username, email=email)
+
 
 @app.route("/forgotPass")
 def forgotPass():
