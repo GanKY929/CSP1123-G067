43101117c42d3ed4485d8cc913fbb8061f354c57
Mithirilz <haqimiamirul829@gmail.com>
2026-05-09 17:18:22 +0800

    Added function to get post's based on ID

--- data_storage.py
+++ data_storage.py
@@ -16,4 +16,23 @@ def save_post(_post_title: str, _post_content:str, _post_owner: int):
     db.session.commit()
 
     return
+
+def get_post(_post_id: int)-> str:
+    if not _post_id:
+        print("Invalid argument")
+        return
+    
+    post_returned = db.session.execute(
+        db.session.query(database.Post).filter_by(post_id = _post_id).first()
+    )
+
+    if not post_returned:
+        print("Error: PostID does not exist")
+        return
+    
+    _post_title = post_returned.post_title
+    _post_owner = post_returned.post_owner
+    _post_content = post_returned.post_content
+
+    return _post_title, _post_owner, _post_content
     
