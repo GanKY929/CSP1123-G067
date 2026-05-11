fa368c739e69dec58c294699b9bf638a04563abb
Mithirilz <haqimiamirul829@gmail.com>
2026-05-11 09:56:53 +0800

    Connected User and Post table via foreign key

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, text, Boolean, DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.schema import ForeignKey
 from datetime import datetime
 from werkzeug.security import check_password_hash as checkPasswordHash, generate_password_hash as generatePasswordHash
 
@@ -27,7 +28,7 @@ class Post(Base):
     post_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     post_title: Mapped[str] = mapped_column(String(200), nullable=False)
     post_content: Mapped[str] = mapped_column(String, nullable=False)
-    post_owner: Mapped[int] = mapped_column(Integer, nullable=False)
+    post_owner: Mapped[int] = mapped_column(Integer, ForeignKey("User.user_id"), nullable=False)
 
 
 def generatePassword(password):
