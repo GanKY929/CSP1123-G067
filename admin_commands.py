23c443dad27fbdf68b105dfce01bf9084843e6d9
Mithirilz <haqimiamirul829@gmail.com>
2026-06-15 14:46:44 +0800

    WIP: Creating admin commands

--- admin_commands.py
+++ admin_commands.py
@@ -2,6 +2,13 @@ from database import db
 from sqlalchemy import select
 import database 
 
-def remove_user():
-    
+def remove_user(_user_id: int):
+    user_to_delete = db.session.query(database.User).filter_by(user_id=_user_id)
+    db.session.delete(user_to_delete)
+
+    return
+
+def verify_user(_user_id: int):
+    user_to_verify = db.session.query(database.User).filter_by(user_id = _user_id)
+    user_to_verify.verified 
     return
